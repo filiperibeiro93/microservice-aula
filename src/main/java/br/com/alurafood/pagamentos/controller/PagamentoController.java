4bcc3000a9cbf72b28253f41c8f1bdbfb1200627
Filipe Ribeiro <facribeiro.93@gmail.com>
2023-07-27 20:53:14 -0300

    endpoint findAll

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -1,13 +1,26 @@
 package br.com.alurafood.pagamentos.controller;
 
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.service.PagamentoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class PagamentoController {
+
+    private final PagamentoService pagamentoService;
 
     @GetMapping
     public String init() {
         return "teste";
+    }
+
+    @GetMapping
+    public List<Pagamento> findAll() {
+        return pagamentoService.findAll();
     }
 }
--- src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -1,7 +1,19 @@
 package br.com.alurafood.pagamentos.service;
 
+import br.com.alurafood.pagamentos.model.Pagamento;
+import br.com.alurafood.pagamentos.repository.PagamentoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class PagamentoService {
+
+    private final PagamentoRepository repository;
+
+    public List<Pagamento> findAll() {
+        return repository.findAll();
+    }
 }
